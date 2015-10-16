a35a8bb2daab05f46d69162a64dbac7cb4345698
André Cibils <andre.cibils@gmail.com>
2015-10-16 17:03:38 +0200

    full of fail but doing the Github stuff

--- app/src/main/java/ch/epfl/sweng/opengm/ch/epfl/sweng/opengm/events/AddRemoveParticipantsActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/ch/epfl/sweng/opengm/events/AddRemoveParticipantsActivity.java
@@ -4,14 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.sweng.opengm.R;
 
 public class AddRemoveParticipantsActivity extends AppCompatActivity {
 
+    private List<OpenGMMember> members;
+    private List<OpenGMMember> membersToAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_remove_participants);
+        membersToAdd = new ArrayList<>();
+        members = new ArrayList<>();
+        members.add(new OpenGMMember());
+        members.add(new OpenGMMember());
+        members.add(new OpenGMMember());
+    }
+
+    public void displayParticipants(){
+
+        ScrollView sv = (ScrollView) findViewById(R.id.scrollViewListOfMember);
+        for(final OpenGMMember m : members){
+            final CheckBox b = new CheckBox(this);
+            b.setText(m.getName());
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (b.isChecked()) {
+                        b.setChecked(false);
+                        membersToAdd.remove(m);
+                    } else {
+                        b.setChecked(true);
+                        membersToAdd.add(m);
+                    }
+                }
+            });
+        sv.addView(b);
+        }
+
+    }
+    private class OpenGMMember{
+        public OpenGMMember(){}
+        public String getName(){
+            return "MemberTester";
+        }
     }
 }
--- app/src/main/java/ch/epfl/sweng/opengm/ch/epfl/sweng/opengm/events/EventListActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/ch/epfl/sweng/opengm/events/EventListActivity.java
@@ -1,11 +1,15 @@
 package ch.epfl.sweng.opengm.ch.epfl.sweng.opengm.events;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 
 import java.util.ArrayList;
@@ -30,12 +34,20 @@ public class EventListActivity extends AppCompatActivity {
         eventList.add(tester2);
         eventList.add(tester3);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
+
         displayEvents();
+
+        setContentView(R.layout.activity_event_list);
+
+
     }
 
     public void displayEvents(){
         ScrollView sv = (ScrollView) findViewById(R.id.scrollViewEventList);
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.LinearLayoytScrollViewEventList);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        ListView lv = (ListView) findViewById(R.id.ListViewScrollViewEventList);
         for(Event e : eventList){
             Button b = new Button(this);
             b.setText(e.getName());
@@ -45,8 +57,10 @@ public class EventListActivity extends AppCompatActivity {
                     //showEvent(); //TODO : gerer ce qu'il se passe quand on clique sur l'event
                 }
             });
-        sv.addView(b);
+        lv.addView(b);
         }
+        sv.addView(linearLayout);
         //TODO : reafficher sv
+
     }
 }
