ba0776131f5fb7b202e61feaa8bd365ad7b545c0
arowbaz <aurelien.soccard@epfl.ch>
2015-10-18 17:38:10 +0200

    Interface with new name has a complete javadoc

--- app/src/main/java/ch/epfl/sweng/opengm/parse/PFImageInterface.java
+++ app/src/main/java/ch/epfl/sweng/opengm/parse/PFImageInterface.java
@@ -8,6 +8,12 @@ import android.graphics.Bitmap;
  */
 public interface PFImageInterface {
 
+    /**
+     * This method must be implemented by each class which implements this interface
+     * so you can save the parameter in the local field of your class.
+     *
+     * @param bitmap The image that you get from a ParseFile and you update in background
+     */
     void setImage(Bitmap bitmap);
 
 }
