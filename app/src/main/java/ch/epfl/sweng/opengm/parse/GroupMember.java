9fc21d169e7f5fc0a2a524c435f0ae115a3f77cc
arowbaz <aurelien.soccard@epfl.ch>
2015-10-18 18:22:40 +0200

    Javadoc complete for Groupmember + make class final

--- app/src/main/java/ch/epfl/sweng/opengm/parse/GroupMember.java
+++ app/src/main/java/ch/epfl/sweng/opengm/parse/GroupMember.java
@@ -16,8 +16,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import ch.epfl.sweng.opengm.identification.ImageOverview;
-
 import static ch.epfl.sweng.opengm.parse.PFConstants.USER_ENTRY_ABOUT;
 import static ch.epfl.sweng.opengm.parse.PFConstants.USER_ENTRY_FIRSTNAME;
 import static ch.epfl.sweng.opengm.parse.PFConstants.USER_ENTRY_GROUPS;
@@ -27,7 +25,12 @@ import static ch.epfl.sweng.opengm.parse.PFConstants.USER_ENTRY_PICTURE;
 import static ch.epfl.sweng.opengm.parse.PFConstants.USER_ENTRY_USERNAME;
 import static ch.epfl.sweng.opengm.parse.PFUtils.retrieveFileFromServer;
 
-public class GroupMember {
+/**
+ * This class represents a member of group : basically this is a user with a username and roles
+ * but we do not download its list of groups (otherwise we may end up with downloading all the groups)
+ * just keep it so we may still be able to add it to the group or remove it.
+ */
+public final class GroupMember {
 
     private final String mId;
     private final List<String> mRoles;
@@ -57,46 +60,101 @@ public class GroupMember {
         this.mGroups = new ArrayList<>(groups);
     }
 
+    /**
+     * Getter for the id of the member
+     *
+     * @return the id associated with this member
+     */
     public String getId() {
         return mId;
     }
 
+    /**
+     * Getter for the username of the member
+     *
+     * @return the username associated with this member
+     */
     public String getUsername() {
         return mUsername;
     }
 
-    public String getSurname() {
+    /**
+     * Getter for the nickname of the member in this group
+     *
+     * @return the nickname associated with this member
+     */
+    public String getNickname() {
         return mNickname;
     }
 
+    /**
+     * Getter for the first name of the member
+     *
+     * @return the first name associated with this member
+     */
     public String getFirstname() {
         return mFirstName;
     }
 
+    /**
+     * Getter for the last name of the member
+     *
+     * @return the last name associated with this member
+     */
     public String getLastname() {
         return mLastName;
     }
 
+    /**
+     * Getter for the phone number of the member
+     *
+     * @return the phone number associated with this member
+     */
     public String getPhoneNumber() {
         return mPhoneNumber;
     }
 
+    /**
+     * Getter for the description of the member
+     *
+     * @return the description associated with this member
+     */
     public String getAbout() {
         return mAboutUser;
     }
 
+    /**
+     * Getter for the roles of the member in this group
+     *
+     * @return the roles associated with this member
+     */
     public List<String> getRoles() {
         return Collections.unmodifiableList(mRoles);
     }
 
+    /**
+     * Getter for the profile picture of the member in this group
+     *
+     * @return the profile picture associated with this member
+     */
     public Bitmap getPicture() {
         return mPicture;
     }
 
-    public void setmNickname(String nickname) {
+    /**
+     * Setter for the nickname of the member in this group
+     *
+     * @param nickname the new nickname of the member (updated by the caller on the server)
+     */
+    public void setNickname(String nickname) {
         this.mNickname = nickname;
     }
 
+    /**
+     * Setter to add the member to a new group
+     *
+     * @param groupId the id of the group that the member will belong to
+     */
     public void addToGroup(String groupId) {
         if (!mGroups.contains(groupId)) {
             mGroups.add(groupId);
@@ -109,6 +167,11 @@ public class GroupMember {
         }
     }
 
+    /**
+     * Setter to remove the member to a new group
+     *
+     * @param groupId the id of the group that the member will be deleted from
+     */
     public void removeFromGroup(String groupId) {
         if (mGroups.contains(groupId)) {
             mGroups.remove(groupId);
@@ -123,12 +186,22 @@ public class GroupMember {
         }
     }
 
+    /**
+     * Setter to add a role to the member
+     *
+     * @param role another role  that will be associated with this member
+     */
     public void addRole(String role) {
         if (!mRoles.contains(role)) {
             mRoles.add(role);
         }
     }
 
+    /**
+     * Setter to remove a role to the member
+     *
+     * @param role a role that will be deleted from the member's role
+     */
     public void removeRole(String role) {
         if (mRoles.contains(role)) {
             mRoles.remove(role);
@@ -137,6 +210,11 @@ public class GroupMember {
         }
     }
 
+    /**
+     * Private method that will update the member on the server : can only updated the list of groups
+     * ths user belong to (eg. if you are a moderator of a group and you want to delete a member you
+     * just need to update this entry not the other ones)
+     */
     private void updateToServer() throws PFException {
         ParseQuery<ParseObject> query = ParseQuery.getQuery(PFConstants.USER_TABLE_NAME);
         query.getInBackground(getId(), new GetCallback<ParseObject>() {
@@ -167,7 +245,7 @@ public class GroupMember {
 
     }
 
-    public static final class Builder implements ImageOverview {
+    public static final class Builder implements PFImageInterface {
 
         private final String mId;
         private final List<String> mRoles;
@@ -181,10 +259,23 @@ public class GroupMember {
         private String mAboutUser;
         private Bitmap mPicture;
 
+        /**
+         * The constructor for a new member that does not have any
+         * nickname or role (default are respectively the username and an empty array)
+         *
+         * @param id the id of the member that we will retrieve information from
+         */
         public Builder(String id) {
             this(id, "", new String[0]);
         }
 
+        /**
+         * The complete constructor of a GroupMember.Builder
+         *
+         * @param id       the id of the member that we will retrieve information from
+         * @param nickname the nickname that will be associated to this member
+         * @param roles    the roles that are attributed ot this user
+         */
         public Builder(String id, String nickname, String[] roles) {
             this.mId = id;
             this.mNickname = nickname;
@@ -192,30 +283,67 @@ public class GroupMember {
             this.mGroups = new ArrayList<>();
         }
 
+        /**
+         * Setter for the username of the member we are building
+         *
+         * @param username the new username of this member
+         */
         private void setUsername(String username) {
             this.mUsername = username;
         }
 
+        /**
+         * Setter for the first name of the member we are building
+         *
+         * @param firstName the new first name of this member
+         */
         private void setFirstName(String firstName) {
             this.mFirstName = firstName;
         }
 
+        /**
+         * Setter for the last name of the member we are building
+         *
+         * @param lastName the new last name of this member
+         */
         private void setLastName(String lastName) {
             this.mLastName = lastName;
         }
 
+        /**
+         * Setter for the phone number of the member we are building
+         *
+         * @param phoneNumber the new phone number of this member
+         */
         private void setPhoneNumber(String phoneNumber) {
             this.mPhoneNumber = phoneNumber;
         }
 
+        /**
+         * Setter for the description of the member we are building
+         *
+         * @param about the new description of this member
+         */
         private void setAbout(String about) {
             this.mAboutUser = about;
         }
 
-        public void setImage(Bitmap image) {
-            this.mPicture = image;
+        /**
+         * Setter for the profile picture of the member we are building
+         *
+         * @param picture the new profile picture of this member
+         */
+        public void setImage(Bitmap picture) {
+            this.mPicture = picture;
         }
 
+        /**
+         * Setter for the list of groups this member belongs to
+         *
+         * @param groupsArray A Json array containing the ids of the groups this member belongs to.
+         * @throws PFException If something when wrong while getting informations from the json array or
+         *                     if the type of the information is not correct
+         */
         private void setGroups(JSONArray groupsArray) throws PFException {
             if (groupsArray != null) {
                 for (int i = 0; i < groupsArray.length(); i++) {
@@ -228,6 +356,11 @@ public class GroupMember {
             }
         }
 
+        /**
+         * A method that retrieves all the informations associated to this member with an id.
+         *
+         * @throws PFException If something went wrong while communicating with the server,
+         */
         protected void retrieveFromServer() throws PFException {
             if (mId == null) {
                 throw new PFException();
@@ -253,6 +386,12 @@ public class GroupMember {
             }
         }
 
+        /**
+         * Builds a new Group member with all its attributes.
+         *
+         * @return a new GroupMember corresponding to the object we were building
+         * @throws PFException If something went wrong while retrieving information online
+         */
         public GroupMember build() throws PFException {
             retrieveFromServer();
             if (mNickname == null || mNickname.isEmpty()) {
