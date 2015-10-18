9c9a07ae5be40411997f60c930ea9f596dcd677f
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-18 14:13:41 +0200

    Add basic behaviour on button click

--- app/src/main/java/ch/epfl/sweng/opengm/CreateGroup.java
+++ app/src/main/java/ch/epfl/sweng/opengm/CreateGroup.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateGroup extends AppCompatActivity {
 
@@ -33,5 +35,13 @@ public class CreateGroup extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createGroup(View view){
+        String groupName = ((EditText) findViewById(R.id.enterGroupName)).getText().toString();
+        String groupDescription = ((EditText) findViewById(R.id.enterGroupDescription)).getText().toString();
+        // TODO : retrieve image from button
+        // TODO : call intent for next activity
+        // If next activity is group page, also call function to put new gorup in the databse
     }
 }
