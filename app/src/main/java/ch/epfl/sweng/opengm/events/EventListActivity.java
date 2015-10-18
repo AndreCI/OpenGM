090bae070f069c345c11df6c68209f2db72279aa
André Cibils <andre.cibils@gmail.com>
2015-10-18 11:22:26 +0200

    EventListActivity works. Skeletton made.

--- app/src/main/java/ch/epfl/sweng/opengm/events/EventListActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/events/EventListActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,18 +30,33 @@ public class EventListActivity extends AppCompatActivity {
         Event tester4 = new Event();
         Event tester5 = new Event();
         Event tester6 = new Event();
+        Event tester7 = new Event();
+        Event tester8 = new Event();
+        Event tester9 = new Event();
+        Event tester10 = new Event();
+        Event tester11 = new Event();
         tester1.setName("E1");
         tester2.setName("E2");
         tester3.setName("E3");
         tester4.setName("E4");
         tester5.setName("E5");
         tester6.setName("E6");
+        tester7.setName("E7");
+        tester8.setName("E8");
+        tester9.setName("E9");
+        tester10.setName("E10");
+        tester11.setName("E11");
         eventList.add(tester1);
         eventList.add(tester2);
         eventList.add(tester3);
         eventList.add(tester4);
         eventList.add(tester5);
         eventList.add(tester6);
+        eventList.add(tester7);
+        eventList.add(tester8);
+        eventList.add(tester9);
+        eventList.add(tester10);
+        eventList.add(tester11);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_event_list);
@@ -46,23 +64,30 @@ public class EventListActivity extends AppCompatActivity {
 
 
     }
+    public void clickOnAddButton(View v){
+        Event toAdd = new Event();
+        toAdd.setName("AddByButton");
+        eventList.add(toAdd);
+        Toast t = Toast.makeText(getApplicationContext(), "Event Added.", Toast.LENGTH_SHORT);
+        t.show();
+        displayEvents();
+    }
 
     public void displayEvents(){
-        ScrollView sv = (ScrollView) findViewById(R.id.scrollViewEventList);
-
+        RelativeLayout r = (RelativeLayout) findViewById(R.id.ScrollViewParentLayout);
+        ScrollView sv = new ScrollView(this);
+        ScrollView.LayoutParams slp = new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT,ScrollView.LayoutParams.MATCH_PARENT);
+        sv.setLayoutParams(slp);
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         linearLayout.setLayoutParams(lp);
-        int i=0;
         for(Event e : eventList){
             final Button b = new Button(this);
             b.setText(e.getName());
-            b.setWidth(40);
-            b.setHeight(40);
-            b.setY(i * 40 + 20);
-            b.setX(40);
+
+            b.setLayoutParams(lp);
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -70,13 +95,11 @@ public class EventListActivity extends AppCompatActivity {
                 }
             });
 
-            i++;
-            // LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-            linearLayout.addView(b, lp);
+            linearLayout.addView(b);
         }
         sv.addView(linearLayout);
-
+        r.addView(sv);
         //TODO : reafficher sv
 
     }
