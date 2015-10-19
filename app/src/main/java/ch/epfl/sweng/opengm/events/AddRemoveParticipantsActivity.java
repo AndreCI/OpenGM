4af25a1bb3bf48178d0b8cda5ae9c30b33168fe9
André Cibils <andre.cibils@gmail.com>
2015-10-19 10:16:49 +0200

    its working fine. Skeleton completed

--- app/src/main/java/ch/epfl/sweng/opengm/events/AddRemoveParticipantsActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/events/AddRemoveParticipantsActivity.java
@@ -27,10 +27,13 @@ public class AddRemoveParticipantsActivity extends AppCompatActivity {
     public static int geneId = 0; //TODO : used for the quickClass to test, delete it later.
     private List<OpenGMMember> members;
     private List<OpenGMMember> membersToAdd;
+    private List<CheckBox> boxes;
+    private LinearLayout linearLayoutListMembers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_remove_participants);
+        boxes = new ArrayList<>();
         membersToAdd = new ArrayList<>();
         members = new ArrayList<>();
         members.add(new OpenGMMember());
@@ -58,6 +61,8 @@ public class AddRemoveParticipantsActivity extends AppCompatActivity {
         members.add(new OpenGMMember());
         members.add(new OpenGMMember());
 
+        linearLayoutListMembers = new LinearLayout(this);
+
         final SearchView sv = (SearchView) findViewById(R.id.searchMember);
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -75,6 +80,21 @@ public class AddRemoveParticipantsActivity extends AppCompatActivity {
             }
         });
         Collections.sort(members, getComparator(""));
+        for(final OpenGMMember m : members){
+            CheckBox c = new CheckBox(this);
+            c.setText(m.getName());
+            c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        membersToAdd.add(m);
+                    } else {
+                        membersToAdd.remove(m);
+                    }
+                }
+            });
+            boxes.add(c);
+        }
         displayParticipants("");
     }
 
@@ -116,34 +136,20 @@ public class AddRemoveParticipantsActivity extends AppCompatActivity {
      * @param query : il query is non empty, it will only show members with the query in their name.
      */
     private void displayParticipants(String query){
-
+        linearLayoutListMembers.removeAllViews();
+        linearLayoutListMembers = new LinearLayout(this);
+        linearLayoutListMembers.setOrientation(LinearLayout.VERTICAL);
         final RelativeLayout memberLayout = (RelativeLayout) findViewById(R.id.memberListLayout);
         memberLayout.removeAllViews();
         ScrollView scrollViewForMembers = new ScrollView(this);
         ScrollView.LayoutParams scrollViewLP = new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT,ScrollView.LayoutParams.MATCH_PARENT);
         scrollViewForMembers.setLayoutParams(scrollViewLP);
-        LinearLayout linearLayoutListMembers = new LinearLayout(this);
-        linearLayoutListMembers.setOrientation(LinearLayout.VERTICAL);
-
-
         LinearLayout.LayoutParams memberListLP = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         linearLayoutListMembers.setLayoutParams(memberListLP);
 
-        for(final OpenGMMember m : members) {
-            if (m.getName().contains(query)) {
-                CheckBox c = new CheckBox(this);
-                c.setText(m.getName());
+        for(CheckBox c : boxes) {
+            if (c.getText().toString().contains(query)) {
                 c.setLayoutParams(memberListLP);
-                c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            membersToAdd.add(m);
-                        } else {
-                            membersToAdd.remove(m);
-                        }
-                    }
-                });
                 linearLayoutListMembers.addView(c);
             }
         }
