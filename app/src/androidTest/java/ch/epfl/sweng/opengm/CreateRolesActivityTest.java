e2cfc67f847b6743905a1be3b1bff4f647aaa178
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-19 14:44:33 +0200

    Add test to add a new role and remove a newly created role

--- app/src/androidTest/java/ch/epfl/sweng/opengm/CreateRolesActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/CreateRolesActivityTest.java
@@ -12,6 +12,9 @@ import android.widget.TextView;
 import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
@@ -48,5 +51,23 @@ public class CreateRolesActivityTest extends ActivityInstrumentationTestCase2<Cr
         onView(withTagValue(is((Object) "removeRole0"))).check(matches(not(isEnabled())));
         onView(withTagValue(is((Object) "removeRole1"))).check(matches(not(isEnabled())));
         onView(withTagValue(is((Object) "removeRole2"))).check(matches(not(isEnabled())));
+    }
+
+    public void testAddNewRoleAndPreserveOldRolesAndState(){
+        onView(withTagValue(is((Object) "addRole"))).perform(click());
+        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
+        onView(withTagValue(is((Object) "okButton"))).perform(click());
+        onView(withTagValue(is((Object) "roleName3"))).check(matches(withText("Super new Role")));
+        onView(withTagValue(is((Object) "removeRole3"))).check(matches(isEnabled()));
+        testInitialThreeRoles();
+        testInitialThreeRolesUnclickable();
+    }
+
+    public void testRemoveAddedRoleAndPreserveOldRolesAndState(){
+        onView(withTagValue(is((Object) "addRole"))).perform(click());
+        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
+        onView(withTagValue(is((Object) "okButton"))).perform(click());
+        onView(withTagValue(is((Object) "removeRole3"))).perform(click());
+        onView(withTagValue(is((Object) "roleRow3"))).check(doesNotExist());
     }
 }
