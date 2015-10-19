6022af27ce528e349f5dfdcbc6793c751df7025e
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-19 14:20:07 +0200

    Add test to check if CreateRoles has the three initial roles that are not removable

--- app/src/androidTest/java/ch/epfl/sweng/opengm/CreateRolesActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/CreateRolesActivityTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.opengm;
 
 import android.app.Activity;
+import android.support.test.espresso.ViewInteraction;
 import android.test.ActivityInstrumentationTestCase2;
 import android.support.test.InstrumentationRegistry;
 import android.view.View;
@@ -12,8 +13,13 @@ import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagValue;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class CreateRolesActivityTest extends ActivityInstrumentationTestCase2<CreateRoles>{
     LinearLayout rolesAndButtons;
@@ -33,6 +39,14 @@ public class CreateRolesActivityTest extends ActivityInstrumentationTestCase2<Cr
     }
 
     public void testInitialThreeRoles() {
+        onView(withTagValue(is((Object) "roleName0"))).check(matches(withText("Administrator")));
+        onView(withTagValue(is((Object) "roleName1"))).check(matches(withText("Moderator")));
+        onView(withTagValue(is((Object) "roleName2"))).check(matches(withText("User")));
+    }
 
+    public void testInitialThreeRolesUnclickable(){
+        onView(withTagValue(is((Object) "removeRole0"))).check(matches(not(isEnabled())));
+        onView(withTagValue(is((Object) "removeRole1"))).check(matches(not(isEnabled())));
+        onView(withTagValue(is((Object) "removeRole2"))).check(matches(not(isEnabled())));
     }
 }
