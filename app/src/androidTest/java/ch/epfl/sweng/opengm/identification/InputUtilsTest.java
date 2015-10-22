eb6e815912fa3700eed3da21915debac8c71520f
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-22 15:09:04 +0200

    Add test for good email address

--- app/src/androidTest/java/ch/epfl/sweng/opengm/identification/InputUtilsTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/identification/InputUtilsTest.java
@@ -6,7 +6,15 @@ import android.test.suitebuilder.annotation.LargeTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static junit.framework.Assert.assertTrue;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class InputUtilsTest {
+
+    @Test
+    public void testAcceptGoodEmail(){
+        assertTrue(InputUtils.isEmailValid("good@mail.com"));
+    }
+
 }
