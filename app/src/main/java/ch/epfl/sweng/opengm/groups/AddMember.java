9b058af0191954dc99513b281a192f7cc2af9bb6
nbonfils <nils.bonfils@epfl.ch>
2015-10-22 13:46:28 +0200

    add AddMember skeleton

---
+++ app/src/main/java/ch/epfl/sweng/opengm/groups/AddMember.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.opengm.groups;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import ch.epfl.sweng.opengm.R;
+
+public class AddMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
