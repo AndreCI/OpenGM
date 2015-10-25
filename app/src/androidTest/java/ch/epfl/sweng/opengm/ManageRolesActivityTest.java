418aac195f574560e371d199eccbac77c85e3ec4
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-25 12:37:11 +0100

    Remove old create roles tests

--- app/src/androidTest/java/ch/epfl/sweng/opengm/ManageRolesActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/ManageRolesActivityTest.java
@@ -35,53 +35,5 @@ public class ManageRolesActivityTest extends ActivityInstrumentationTestCase2<Ma
         rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
     }
 
-    public void testInitialThreeRoles() {
-        onView(withTagValue(is((Object) "roleName0"))).check(matches(withText("Administrator")));
-        onView(withTagValue(is((Object) "roleName1"))).check(matches(withText("Moderator")));
-        onView(withTagValue(is((Object) "roleName2"))).check(matches(withText("User")));
-    }
 
-    public void testInitialThreeRolesUnclickable(){
-        onView(withTagValue(is((Object) "removeRole0"))).check(matches(not(isEnabled())));
-        onView(withTagValue(is((Object) "removeRole1"))).check(matches(not(isEnabled())));
-        onView(withTagValue(is((Object) "removeRole2"))).check(matches(not(isEnabled())));
-    }
-
-    public void testAddNewRoleAndPreserveOldRolesAndState(){
-        onView(withTagValue(is((Object) "addRole"))).perform(click());
-        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
-        onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "roleName3"))).check(matches(withText("Super new Role")));
-        onView(withTagValue(is((Object) "removeRole3"))).check(matches(isEnabled()));
-        testInitialThreeRoles();
-        testInitialThreeRolesUnclickable();
-    }
-
-    public void testRemoveAddedRoleAndPreserveOldRolesAndState(){
-        onView(withTagValue(is((Object) "addRole"))).perform(click());
-        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
-        onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "removeRole3"))).perform(click());
-        onView(withTagValue(is((Object) "roleRow3"))).check(doesNotExist());
-
-        testInitialThreeRolesUnclickable();
-        testInitialThreeRoles();
-    }
-
-    public void testAddTwoRolesAndRemoveOnlyOne(){
-        onView(withTagValue(is((Object) "addRole"))).perform(click());
-        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
-        onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "addRole"))).perform(click());
-        onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Another super new Role"));
-        onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "removeRole4"))).perform(click());
-        onView(withTagValue(is((Object) "roleRow4"))).check(doesNotExist());
-        onView(withTagValue(is((Object) "roleName3"))).check(matches(withText("Super new Role")));
-    }
-
-    public void testCantPressAddButtonWhenEditingNewRole(){
-        onView(withTagValue(is((Object) "addRole"))).perform(click());
-        onView(withTagValue(is((Object) "addRole"))).check(doesNotExist());
-    }
 }
