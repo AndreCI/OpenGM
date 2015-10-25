3ad58bc5ea83acb1b66548e08f71a328af440b8f
nbonfils <nils.bonfils@epfl.ch>
2015-10-25 19:50:53 +0100

    add action bar and add person button to the Members activity

--- app/src/main/java/ch/epfl/sweng/opengm/groups/Members.java
+++ app/src/main/java/ch/epfl/sweng/opengm/groups/Members.java
@@ -3,6 +3,8 @@ package ch.epfl.sweng.opengm.groups;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import ch.epfl.sweng.opengm.parse.PFUser;
 public class Members extends AppCompatActivity {
 
     private ListView list;
+    private PFGroup group;
     private List<PFMember> members;
 
     public static final String GROUP_ID = "ch.epfl.sweng.opengm.groups.members.groupid";
@@ -26,22 +29,47 @@ public class Members extends AppCompatActivity {
         setContentView(R.layout.activity_members);
 
 //        int groupId = getIntent().getIntExtra(GROUP_ID, -1);
-//        members = OpenGMApplication.getCurrentUser().getGroups().get(groupId).getMembers();
+//        group = OpenGMApplication.getCurrentUser().getGroups().get(groupId);
+//        members = group.getMembers();
 
         // hardcoded the getting of user for tests
         int groupId = 0;
+        PFUser user = null;
         try {
-            PFUser user = PFUser.fetchExistingUser("oqMblls8Cb");
-            List<PFGroup> groups = user.getGroups();
-            members = groups.get(groupId).getMembers();
+            user = PFUser.fetchExistingUser("oqMblls8Cb");
         } catch (PFException e) {
             e.printStackTrace();
         }
+        group = user.getGroups().get(groupId);
+        members = group.getMembers();
         //-----------------------------------------
 
         list = (ListView) findViewById(R.id.member_list);
 
         MembersAdapter adapter = new MembersAdapter(this, R.layout.item_member, members);
         list.setAdapter(adapter);
+
+        setTitle(group.getName());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_members, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_person:
+                addPerson();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void addPerson() {
+
     }
 }
