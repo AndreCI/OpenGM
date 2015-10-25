81b7a2cc4b816149933392b5a98519b25dc8c997
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-25 16:53:57 +0100

    Add test to see if group name is too short

--- app/src/androidTest/java/ch/epfl/sweng/opengm/CreateGroupActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/CreateGroupActivityTest.java
@@ -1,18 +1,36 @@
 package ch.epfl.sweng.opengm;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 
 import ch.epfl.sweng.opengm.groups.CreateGroup;
 import ch.epfl.sweng.opengm.parse.PFUser;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagValue;
+import static org.hamcrest.Matchers.is;
 
 public class CreateGroupActivityTest extends ActivityInstrumentationTestCase2<CreateGroup>{
     Activity createGroupActivity;
     PFUser currentUser;
 
-    public CreateGroupActivityTest(Class activityClass) {
+    public CreateGroupActivityTest() {
         super(CreateGroup.class);
     }
 
@@ -23,5 +41,40 @@ public class CreateGroupActivityTest extends ActivityInstrumentationTestCase2<Cr
         createGroupActivity = getActivity();
         currentUser = PFUser.fetchExistingUser("f9PMNCFLXN");
         OpenGMApplication.setCurrentUser(currentUser.getId());
+    }
+
+    public void testDeclinesTooShortName() throws InterruptedException {
+        onView(withId(R.id.enterGroupName)).perform(typeText("sh"));
+        closeSoftKeyboard();
+        Thread.sleep(1000);
+        onView(withId(R.id.doneGroupCreate)).perform(click());
+
+        onView(withId(R.id.enterGroupName)).check(matches(hasErrorText("Group name is too short")));
+    }
+
+    private BaseMatcher<View> hasErrorText(final String expectedError){
+        return new BaseMatcher<View>() {
+            @Override
+            public boolean matches(Object item) {
+                boolean toReturn;
+                if(item.getClass() != View.class){
+                    toReturn = false;
+                } else {
+                    View toTest = (View) item;
+                    if(toTest instanceof EditText){
+                        EditText editTextToTest = (EditText) toTest;
+                        toReturn = expectedError.equals(editTextToTest.getError().toString());
+                    } else {
+                        toReturn = false;
+                    }
+                }
+                return toReturn;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+        };
     }
 }
