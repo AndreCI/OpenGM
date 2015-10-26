5285ee562d28d06fe30017b595c3ddeeeaa73f8e
bivbi <virgile.neu@epfl.ch>
2015-10-26 14:12:10 +0100

    started to make addRemoveParticipant GUI better

---
+++ app/src/main/java/ch/epfl/sweng/opengm/events/CheckParticipant.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.opengm.events;
+
+/**
+ * Created by virgile on 26/10/2015.
+ */
+public class CheckParticipant {
+    private String mParticipantName;
+    private boolean mIsCheck;
+
+    public String getName() {
+        return mParticipantName;
+    }
+
+    public void setName(String mParticipantName) {
+        this.mParticipantName = mParticipantName;
+    }
+
+    public boolean getCheck() {
+        return mIsCheck;
+    }
+
+    public void setCheck(boolean mIsCheck) {
+        this.mIsCheck = mIsCheck;
+    }
+
+    public CheckParticipant(String participantName, boolean isCheck) {
+        mParticipantName = participantName;
+
+        mIsCheck = isCheck;
+    }
+}
