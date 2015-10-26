9bca3092c557ec23237808f7d65d92398fd07998
Alexandre Connat <alexandre.connat@epfl.ch>
2015-10-26 14:49:33 +0100

    Add Test class for PFUser model

--- app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFUserTest.java
@@ -1,7 +1,15 @@
 package ch.epfl.sweng.opengm.parse;
 
-/**
- * Created by Alexandre on 26.10.15.
- */
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class PFUserTest {
+
+
+
 }
