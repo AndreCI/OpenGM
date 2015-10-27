9bde6256ca10082323a1961feeefbaaf772a8c5b
Alexandre Connat <alexandre.connat@epfl.ch>
2015-10-27 01:42:51 +0100

    Add tests for basic getters + members getters in PFGroup

--- app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFGroupTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFGroupTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.opengm.parse;
 
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -9,6 +10,12 @@ import com.parse.ParseQuery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -46,6 +53,54 @@ public class PFGroupTest {
         }
     }
 
+    private void deleteGroupWithName(String name) {
+        try {
+            // Remove from Group table
+            ParseQuery<ParseObject> query = ParseQuery.getQuery(PFConstants.GROUP_TABLE_NAME);
+            query.whereEqualTo(PFConstants.GROUP_ENTRY_NAME, name);
+            ParseObject group = query.getFirst();
+            group.delete();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void getNameAndDescriptionTest() throws PFException {
+        PFUser user = createTestUserWithID(USER_ID + "0");
+        PFGroup group = PFGroup.createNewGroup(user, "Really Nice Group", "A group, much nicer than the previous one", null);
+
+        List<PFGroup> groups = user.getGroups();
+        assertEquals(group, groups.get(0));
+
+        assertEquals("Really Nice Group", group.getName());
+        assertEquals("A group, much nicer than the previous one", group.getDescription());
+
+        deleteUserWithId(USER_ID + "0");
+        deleteGroupWithName("Really Nice Group");
+    }
+
+    @Test
+    public void getMembersTest() throws PFException {
+        PFUser user = createTestUserWithID(USER_ID + "a");
+        PFUser user2 = createTestUserWithID(USER_ID + "b");
+
+        PFGroup group = PFGroup.createNewGroup(user, "Another Group", "Can we add members to this group ?", null);
+        group.addUser(USER_ID + "b");
+
+        List<PFMember> members = new ArrayList<>(Arrays.asList(PFMember.fetchExistingMember(USER_ID + "a"),
+                                                               PFMember.fetchExistingMember(USER_ID + "b")));
+        Collections.reverse(members);
+        // Contains same members, in reverse order
+        assertEquals(members, group.getMembers());
+
+        List<PFMember> membersAlone = new ArrayList<>();
+        membersAlone.add(PFMember.fetchExistingMember(USER_ID + "b"));
+        assertEquals(membersAlone, group.getMembersWithoutUser(USER_ID + "a"));
+
+        deleteUserWithId(USER_ID + "a");
+        deleteUserWithId(USER_ID + "b");
+        deleteGroupWithName("Another Group");
+    }
 
 }
--- app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFMemberTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/parse/PFMemberTest.java
@@ -22,13 +22,18 @@ public class PFMemberTest {
     private final String USERNAME = "BobTheBobby";
     private final String FIRST_NAME = "Bobby";
     private final String LAST_NAME = "LaPointe";
+    private final String ABOUT_TEXT = "A simple about text";
+    private final String PHONE_NUMBER = "0793332567";
 
 
     private PFUser createTestUserWithID(String id) {
         PFUser userTest = null;
         try {
             userTest = PFUser.createNewUser(id, EMAIL, USERNAME, FIRST_NAME, LAST_NAME);
-        } catch (PFException e) {
+            Thread.sleep(1000);
+            userTest.setAboutUser(ABOUT_TEXT);
+            userTest.setPhoneNumber(PHONE_NUMBER);
+        } catch (PFException | InterruptedException e) {
             e.printStackTrace();
         }
         return userTest;
@@ -46,6 +51,24 @@ public class PFMemberTest {
         }
     }
 
+    @Test
+    public void gettersTest() throws PFException, InterruptedException {
+        createTestUserWithID(USER_ID);
+        PFMember member = PFMember.fetchExistingMember(USER_ID);
+        Thread.sleep(1000);
 
+        assertEquals(USERNAME, member.getUsername());
+        // TODO: getter for email ?
+//        assertEquals(EMAIL, member.getEmail());
+        assertEquals(FIRST_NAME, member.getFirstname());
+        assertEquals(LAST_NAME, member.getLastname());
+        assertEquals(PHONE_NUMBER, member.getPhoneNumber());
+        assertEquals(ABOUT_TEXT, member.getAbout());
+//        member.getPicture();
+        // TODO: test roles
+//        member.getRoles();
+
+        deleteUserWithId(USER_ID);
+    }
 
 }
