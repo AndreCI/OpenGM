5e9a45f65795c17a24998a824af650eab9d7635d
Radoslaw Dryzner <radoslaw.dryzner@epfl.ch>
2015-10-29 20:36:02 +0100

    Rewrite tests to make them independant of parse database status

--- app/src/androidTest/java/ch/epfl/sweng/opengm/ManageRolesActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/ManageRolesActivityTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.opengm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
@@ -9,15 +10,22 @@ import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import org.junit.Test;
 
 import java.nio.charset.CharacterCodingException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.sweng.opengm.groups.ManageRoles;
+import ch.epfl.sweng.opengm.parse.PFConstants;
 import ch.epfl.sweng.opengm.parse.PFException;
 import ch.epfl.sweng.opengm.parse.PFGroup;
+import ch.epfl.sweng.opengm.parse.PFMember;
 import ch.epfl.sweng.opengm.parse.PFUser;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -36,8 +44,8 @@ import static org.hamcrest.Matchers.not;
 public class ManageRolesActivityTest extends ActivityInstrumentationTestCase2<ManageRoles>{
     LinearLayout rolesAndButtons;
     Activity createRolesActivity;
-    PFUser testUser;
     PFGroup testGroup;
+    List<PFUser> testUsers;
 
     public ManageRolesActivityTest() {
         super(ManageRoles.class);
@@ -47,40 +55,50 @@ public class ManageRolesActivityTest extends ActivityInstrumentationTestCase2<Ma
     public void setUp() throws Exception{
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        createRolesActivity = getActivity();
-
-        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
-        setUpDatabaseInfo();
     }
 
-    private void setUpDatabaseInfo() throws PFException {
-        testUser = PFUser.fetchExistingUser("f9PMNCFLXN");
-        testGroup = PFGroup.fetchExistingGroup("9E0kzVZF4i");
-        addTestRoles();
+    private void launchActivityWithIntent(int numUser) throws PFException {
+        Intent intent = new Intent();
+        setUpTestingEnvironment(numUser);
+
+        ArrayList<String> testUsersIds = new ArrayList<>();
+        for(PFUser testUser : testUsers){
+            testUsersIds.add(testUser.getId());
+        }
+
+        intent.putStringArrayListExtra(ManageRoles.USER_IDS, testUsersIds);
+        intent.putExtra(ManageRoles.GROUP_ID, testGroup.getId());
+
+        setActivityIntent(intent);
     }
 
-    private void addTestRoles(){
-        testGroup.addRoleToUser("TestRole1", testUser.getId());
-        testGroup.addRoleToUser("TestRole2", testUser.getId());
-    }
-
-    private void cleanUpTestRoles(){
-        List<String> roles = new ArrayList<>(testGroup.getRolesForUser(testUser.getId()));
-        for(String role : roles){
-            testGroup.removeRoleToUser(role, testUser.getId());
+    private void setUpTestingEnvironment(int numUser) throws PFException {
+        testUsers = new ArrayList<>();
+        PFUser firstUser = PFUser.createNewUser("testUser" + 0, "testEmail" + 0 + "@test.com", "testUsername" + 0, "testFirst" + 0, "testLast" + 0);
+        testUsers.add(firstUser);
+        testGroup = PFGroup.createNewGroup(firstUser, "TestGroupManageRoles", "Test Group", null);
+        for(int i = 1; i < numUser; i++){
+            testUsers.add(PFUser.createNewUser("testUser" + i, "testEmail" + i + "@test.com", "testUsername" + i, "testFirst" + i, "testLast" + i));
+            testGroup.addUser("testUser" + i);
         }
     }
 
+    private void cleanUpDBAfterTests() throws com.parse.ParseException, InterruptedException {
+        for(PFUser testUser : testUsers){
+            deleteUserFromDatabase(testUser.getId());
+        }
+        deleteGroupFromDatabase(testGroup.getId());
+    }
+
     private boolean databaseRolesMatchesView() throws PFException {
-        setUpDatabaseInfo();
-        List<String> roles = new ArrayList<>(testGroup.getRolesForUser(testUser.getId()));
+        List<String> roles = getRolesIntersection();
         boolean allIn = true;
 
-        for(int i = 0; i < rolesAndButtons.getChildCount(); i++){
+        for(int i = 0; i < rolesAndButtons.getChildCount(); i++) {
             TableRow currentRow = (TableRow) rolesAndButtons.getChildAt(i);
-            if(currentRow.getChildCount() > 1){
+            if (currentRow.getChildCount() > 1) {
                 TextView currentRole = (TextView) currentRow.getChildAt(1);
-                if(!roles.contains(currentRole.getText().toString())){
+                if (!roles.contains(currentRole.getText().toString())) {
                     allIn = false;
                 } else {
                     roles.remove(currentRole.getText().toString());
@@ -90,11 +108,52 @@ public class ManageRolesActivityTest extends ActivityInstrumentationTestCase2<Ma
         return roles.isEmpty() && allIn;
     }
 
-    public void testIfFetchesUsersRoles() throws PFException {
-        assertTrue(databaseRolesMatchesView());
+    private void updateRefsToDB() throws Exception{
+        testGroup = PFGroup.fetchExistingGroup(testGroup.getId());
+        for(int i = 0; i < testUsers.size(); i++){
+            testUsers.set(i, PFUser.fetchExistingUser(testUsers.get(i).getId()));
+        }
     }
 
-    public void testIfFetchedRolesChecked(){
+    private List<String>getRolesIntersection(){
+        List<String> roles = new ArrayList<>(testGroup.getRolesForUser(testUsers.get(0).getId()));
+        ArrayList<String> toRemove = new ArrayList<>();
+        for(int i = 0; i < testUsers.size(); i++){
+            for(String role : roles){
+                List<String> otherRoles = testGroup.getRolesForUser(testUsers.get(i).getId());
+                if(!otherRoles.contains(role)){
+                    toRemove.add(role);
+                }
+            }
+        }
+
+        roles.removeAll(toRemove);
+        return roles;
+    }
+
+    public void testIfFetchesUsersRoles() throws PFException, InterruptedException, com.parse.ParseException {
+        launchActivityWithIntent(1);
+        for(int i = 0; i < 3; i++){
+            testGroup.addRoleToUser("testRole" + i, testUsers.get(0).getId());
+        }
+        Thread.sleep(1000);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
+
+        assertTrue(databaseRolesMatchesView());
+        cleanUpDBAfterTests();
+        Thread.sleep(1000);
+    }
+
+    public void testIfFetchedRolesChecked() throws Exception{
+        launchActivityWithIntent(1);
+        for(int i = 0; i < 3; i++){
+            testGroup.addRoleToUser("testRole" + i, testUsers.get(0).getId());
+        }
+        Thread.sleep(1000);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
+
         boolean allChecked = true;
         for(int i = 0; i < rolesAndButtons.getChildCount(); i++){
             TableRow currentRow = (TableRow) rolesAndButtons.getChildAt(i);
@@ -106,51 +165,107 @@ public class ManageRolesActivityTest extends ActivityInstrumentationTestCase2<Ma
             }
         }
         assertTrue(allChecked);
+        cleanUpDBAfterTests();
+        Thread.sleep(1000);
     }
 
-    public void testIfAddsRoles() throws PFException {
+    public void testIfAddsRoles() throws Exception {
+        launchActivityWithIntent(1);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
         onView(withTagValue(is((Object) "addRole"))).perform(click());
         onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
         onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "roleName2"))).check(matches(withText("Super new Role")));
-        onView(withTagValue(is((Object) "removeRole2"))).check(matches(isEnabled()));
-        onView(withTagValue(is((Object) "roleBox2"))).check(matches(isChecked()));
-        onView(withTagValue(is((Object) "roleBox2"))).check(matches(not(isEnabled())));
+        onView(withTagValue(is((Object) "roleName0"))).check(matches(withText("Super new Role")));
+        onView(withTagValue(is((Object) "removeRole0"))).check(matches(isEnabled()));
+        onView(withTagValue(is((Object) "roleBox0"))).check(matches(isChecked()));
+        onView(withTagValue(is((Object) "roleBox0"))).check(matches(not(isEnabled())));
         onView(withId(R.id.button)).perform(click());
+        Thread.sleep(1000);
+        updateRefsToDB();
+        Thread.sleep(1000);
 
         assertTrue(databaseRolesMatchesView());
-        testGroup.removeRoleToUser("Super new Role", testUser.getId());
+        cleanUpDBAfterTests();
+        Thread.sleep(1000);
     }
 
-    public void testIfRemovingAddedRoleDoesntGoToDatabase() throws PFException {
+
+    public void testIfRemovingAddedRoleDoesntGoToDatabase() throws Exception {
+        launchActivityWithIntent(1);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
         onView(withTagValue(is((Object) "addRole"))).perform(click());
         onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText("Super new Role"));
         onView(withTagValue(is((Object) "okButton"))).perform(click());
-        onView(withTagValue(is((Object) "roleName2"))).check(matches(withText("Super new Role")));
-        onView(withTagValue(is((Object) "removeRole2"))).check(matches(isEnabled()));
-        onView(withTagValue(is((Object) "removeRole2"))).perform(click());
-        onView(withTagValue(is((Object) "roleName2"))).check(doesNotExist());
+        onView(withTagValue(is((Object) "roleName0"))).check(matches(withText("Super new Role")));
+        onView(withTagValue(is((Object) "removeRole0"))).check(matches(isEnabled()));
+        onView(withTagValue(is((Object) "removeRole0"))).perform(click());
+        onView(withTagValue(is((Object) "roleName0"))).check(doesNotExist());
+
         onView(withId(R.id.button)).perform(click());
-        assertTrue(databaseRolesMatchesView());
+        Thread.sleep(1000);
+        updateRefsToDB();
+        Thread.sleep(1000);
+        boolean result = databaseRolesMatchesView();
+        cleanUpDBAfterTests();
+        assertTrue(result);
     }
 
-    public void testIfRemovesRoleFromDatabase() throws PFException {
-        onView(withTagValue(is((Object) "roleBox1"))).perform(click());
+
+    public void testIfRemovesRoleFromDatabase() throws Exception {
+        launchActivityWithIntent(1);
+        testGroup.addRoleToUser("testRole0", testUsers.get(0).getId());
+        Thread.sleep(1000);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
+
+        onView(withTagValue(is((Object) "roleBox0"))).perform(click());
         onView(withId(R.id.button)).perform(click());
-        testGroup = PFGroup.fetchExistingGroup(testGroup.getId());
-        assertTrue(!testGroup.getRolesForUser(testUser.getId()).contains("TestRole1"));
-        addTestRoles();
+        Thread.sleep(1000);
+        updateRefsToDB();
+        Thread.sleep(1000);
+        boolean result = !testGroup.getRolesForUser(testUsers.get(0).getId()).contains("testRole0");
+        cleanUpDBAfterTests();
+        Thread.sleep(1000);
+        assertTrue(result);
     }
 
-    public void testIfDeclinesBadRoleName() {
+
+    public void testIfDeclinesBadRoleName() throws Exception{
+        launchActivityWithIntent(1);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
         onView(withTagValue(is((Object) "addRole"))).perform(click());
         onView(withTagValue(is((Object) "newRoleEdit"))).perform(typeText(""));
         onView(withTagValue(is((Object) "okButton"))).perform(click());
         onView(withTagValue(is((Object) "roleName2"))).check(doesNotExist());
+        cleanUpDBAfterTests();
     }
 
-    public void testCantPressAddButtonWhenEditingNewRole(){
+
+    public void testCantPressAddButtonWhenEditingNewRole() throws Exception{
+        launchActivityWithIntent(1);
+        createRolesActivity = getActivity();
+        rolesAndButtons = (LinearLayout)createRolesActivity.findViewById(R.id.rolesAndButtons);
         onView(withTagValue(is((Object) "addRole"))).perform(click());
         onView(withTagValue(is((Object) "addRole"))).check(doesNotExist());
+        cleanUpDBAfterTests();
+    }
+
+    private void deleteUserFromDatabase(String id) throws com.parse.ParseException, InterruptedException {
+        ParseQuery<ParseObject> query1 = ParseQuery.getQuery(PFConstants.USER_TABLE_NAME);
+        query1.whereEqualTo(PFConstants.USER_ENTRY_USERID, id);
+        ParseObject user1 = query1.getFirst();
+        user1.delete();
+        Thread.sleep(1000);
+    }
+
+    private void deleteGroupFromDatabase(String id) throws com.parse.ParseException, InterruptedException {
+        ParseQuery<ParseObject> query1 = ParseQuery.getQuery(PFConstants.GROUP_TABLE_NAME);
+        query1.whereEqualTo(PFConstants.OBJECT_ID, id);
+        ParseObject user1 = query1.getFirst();
+        user1.delete();
+        Thread.sleep(1000);
     }
 }
