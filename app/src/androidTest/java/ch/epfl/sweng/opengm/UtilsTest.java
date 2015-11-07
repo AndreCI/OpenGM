1bb5128cbe36d37497e379fec9dc6e2c2b85a47b
bivbi <virgile.neu@epfl.ch>
2015-11-07 14:40:43 +0100

    test zip and unzip roles

--- app/src/androidTest/java/ch/epfl/sweng/opengm/UtilsTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/UtilsTest.java
@@ -7,11 +7,16 @@ import com.parse.ParseUser;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import ch.epfl.sweng.opengm.parse.PFConstants;
 
 import static ch.epfl.sweng.opengm.utils.Utils.stripAccents;
+import static ch.epfl.sweng.opengm.utils.Utils.unzipRoles;
+import static ch.epfl.sweng.opengm.utils.Utils.zipRole;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class UtilsTest {
@@ -47,5 +52,48 @@ public class UtilsTest {
         String expecteed = "aaaeeeeiiiooouuyc";
         assertEquals(expecteed, stripAccents(input));
     }
+
+    @Test
+    public void zipRolesTest() {
+        List<String> l = new ArrayList<>();
+        String excepted = "";
+        for (int i = 0; i < 10; ++i) {
+            String s = "role" + Integer.toString(i);
+            l.add(s);
+            excepted += "role" + Integer.toString(i) + " -- ";
+        }
+        excepted = excepted.substring(0, excepted.length() - 4);
+        String s = zipRole(l);
+        assertEquals(excepted, s);
+    }
+
+    @Test
+    public void unzipRolesTest() {
+        List<String> l = new ArrayList<>();
+        List<String[]> expected = new ArrayList<>();
+        for (int i = 0; i < 10; ++i) {
+            String[] tab = new String[3];
+            String s = "";
+            for (int j = 0; j < 2; ++j) {
+                String r = "role" + Integer.toString(i + j);
+                tab[j] = r;
+                s += r + " -- ";
+            }
+            String r = "role" + Integer.toString(i + 2);
+            s += r;
+            tab[2] = r;
+            expected.add(tab);
+            l.add(s);
+        }
+
+        List<String[]> result = unzipRoles(l);
+
+        for(int i = 0; i < l.size(); ++i) {
+            for(int j = 0; j < 3; ++j) {
+                assertEquals(expected.get(i)[j], result.get(i)[j]);
+            }
+        }
+    }
+
 
 }
