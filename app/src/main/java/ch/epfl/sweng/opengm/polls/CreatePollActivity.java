6d6156d140cbd9418d57bd272ffe7baa79a273ed
arowbaz <aurelien.soccard@epfl.ch>
2015-11-18 23:16:55 +0100

    Basic XML layout

--- app/src/main/java/ch/epfl/sweng/opengm/polls/CreatePollActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/polls/CreatePollActivity.java
@@ -1,31 +1,52 @@
 package ch.epfl.sweng.opengm.polls;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.NumberPicker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.sweng.opengm.R;
 
 public class CreatePollActivity extends AppCompatActivity {
 
+    private NumberPicker possibleAnswersPicker;
+    private List<String> answers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_poll);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        answers = new ArrayList<>();
+
+        possibleAnswersPicker = (NumberPicker) findViewById(R.id.possibleAnswersPicker);
+        possibleAnswersPicker.setMinValue(1);
+        possibleAnswersPicker.setValue(1);
+        possibleAnswersPicker.setMaxValue(1);
+        possibleAnswersPicker.setWrapSelectorWheel(false);
+        possibleAnswersPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                // TODO Auto-generated method stub
+
+                String Old = "Old Value : ";
+                String New = "New Value : ";
+
+                Log.d("OLD", Old.concat(String.valueOf(oldVal)));
+                Log.d("NEW", New.concat(String.valueOf(newVal)));
             }
         });
+    }
+
+    public void addAnswer(View view) {
+        answers.add("");
+        possibleAnswersPicker.setMaxValue(answers.size() + 1);
+        Log.d("OLD", "CLICK");
     }
 
 }
