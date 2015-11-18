c3b2e4a2c544ad02207d0730d50265ffb5081ddc
arowbaz <aurelien.soccard@epfl.ch>
2015-11-18 22:47:46 +0100

    Track of the files that must be completed for this feature

---
+++ app/src/main/java/ch/epfl/sweng/opengm/polls/PollVoteActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.opengm.polls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.opengm.R;
+
+public class PollVoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poll_vote);
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/opengm/polls/PollsListActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.opengm.polls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.opengm.R;
+
+public class PollsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polls_list);
+    }
+}
