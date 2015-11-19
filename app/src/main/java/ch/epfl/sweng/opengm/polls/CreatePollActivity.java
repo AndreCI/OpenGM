7cdb6b469db52fc0d91134d086743a74ee3d7a6d
arowbaz <aurelien.soccard@epfl.ch>
2015-11-19 09:21:04 +0100

    Some XML improved design

--- app/src/main/java/ch/epfl/sweng/opengm/polls/CreatePollActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/polls/CreatePollActivity.java
@@ -2,9 +2,8 @@ package ch.epfl.sweng.opengm.polls;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,40 +12,31 @@ import ch.epfl.sweng.opengm.R;
 
 public class CreatePollActivity extends AppCompatActivity {
 
-    private NumberPicker possibleAnswersPicker;
-    private List<String> answers;
+    private int possibleAnswers = 0;
+    private final List<String> answers = new ArrayList<>();
+
+    private TextView nOfAnswersText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_poll);
-
-        answers = new ArrayList<>();
-
-        possibleAnswersPicker = (NumberPicker) findViewById(R.id.possibleAnswersPicker);
-        possibleAnswersPicker.setMinValue(1);
-        possibleAnswersPicker.setValue(1);
-        possibleAnswersPicker.setMaxValue(1);
-        possibleAnswersPicker.setWrapSelectorWheel(false);
-        possibleAnswersPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                // TODO Auto-generated method stub
-
-                String Old = "Old Value : ";
-                String New = "New Value : ";
-
-                Log.d("OLD", Old.concat(String.valueOf(oldVal)));
-                Log.d("NEW", New.concat(String.valueOf(newVal)));
-            }
-        });
+        nOfAnswersText = (TextView) findViewById(R.id.nOfAnswers_textView);
+        nOfAnswersText.setText(String.format("%d", possibleAnswers));
     }
 
-    public void addAnswer(View view) {
-        answers.add("");
-        possibleAnswersPicker.setMaxValue(answers.size() + 1);
-        Log.d("OLD", "CLICK");
+    public void increasePossibleAnswers(View view) {
+        if (possibleAnswers < answers.size() + 1) {
+            possibleAnswers++;
+            nOfAnswersText.setText(String.format("%d", possibleAnswers));
+        }
+    }
+
+    public void decreasePossibleAnswers(View view) {
+        if (possibleAnswers > 1) {
+            possibleAnswers--;
+            nOfAnswersText.setText(String.format("%d", possibleAnswers));
+        }
     }
 
 }
