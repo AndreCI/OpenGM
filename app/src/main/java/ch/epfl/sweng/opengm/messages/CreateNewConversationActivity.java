2c7be4bff40d018bc39fb81edc717fb5c16ac86c
bivbi <virgile.neu@epfl.ch>
2015-11-20 17:33:22 +0100

    asyncktask for creating new file

--- app/src/main/java/ch/epfl/sweng/opengm/messages/CreateNewConversationActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/messages/CreateNewConversationActivity.java
@@ -1,10 +1,14 @@
 package ch.epfl.sweng.opengm.messages;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -20,6 +24,19 @@ public class CreateNewConversationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_new_conversation);
         Intent intent = getIntent();
         currentGroup = intent.getParcelableExtra(ch.epfl.sweng.opengm.events.Utils.GROUP_INTENT_MESSAGE);
+
+        EditText editText = (EditText) findViewById(R.id.newConversationName);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    sendBackResult();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
 
@@ -30,8 +47,9 @@ public class CreateNewConversationActivity extends AppCompatActivity {
     private void sendBackResult() {
         Intent intent = new Intent();
         String conversationName = ((EditText) findViewById(R.id.newConversationName)).getText().toString();
-        File file = new File(getFilesDir(), conversationName + ".txt");
-        intent.putExtra(Utils.CONVERSATION_INFO_INTENT_MESSAGE, new ConversationInformation(conversationName, currentGroup.getId(), file.getAbsolutePath()));
+        String path = getFilesDir().getAbsolutePath()+conversationName+".txt";
+        new CreateFileInBackground().execute(path);
+        intent.putExtra(Utils.CONVERSATION_INFO_INTENT_MESSAGE, new ConversationInformation(conversationName, currentGroup.getId(), path));
         setResult(ShowConversationsActivity.NEW_CONVERSATION_REQUEST_CODE, intent);
         finish();
     }
@@ -40,4 +58,14 @@ public class CreateNewConversationActivity extends AppCompatActivity {
     public void onBackPressed() {
         sendBackResult();
     }
+
+    class CreateFileInBackground extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            new File(getFilesDir(), params[0]);
+            return null;
+        }
+    }
+
 }
