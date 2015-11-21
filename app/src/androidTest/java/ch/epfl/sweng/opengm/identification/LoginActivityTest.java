ab4e0f851e295ff50cc0945c6709e6d9e64d5611
arowbaz <aurelien.soccard@epfl.ch>
2015-11-21 09:36:08 +0100

    Changing user login since the one used already belongs to some groups ...

--- app/src/androidTest/java/ch/epfl/sweng/opengm/identification/LoginActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/identification/LoginActivityTest.java
@@ -28,7 +28,7 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
     private final static String CURRENT_DATE = "" + Calendar.getInstance().getTimeInMillis();
 
     private final static String USERNAME_INCORRECT = "user" + CURRENT_DATE;
-    private final static String USERNAME_CORRECT = "aurel";
+    private final static String USERNAME_CORRECT = "root";
     private final static String PASSWORD_CORRECT = "Password1";
 
     public LoginActivityTest() {
