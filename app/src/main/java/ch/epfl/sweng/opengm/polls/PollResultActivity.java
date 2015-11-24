12b415152d654e6c0811bd6aa9a85f36a658c6e8
arowbaz <aurelien.soccard@epfl.ch>
2015-11-24 11:16:00 +0100

    Add the library to display results nicely + solved conflits while importing

--- app/src/main/java/ch/epfl/sweng/opengm/polls/PollResultActivity.java
+++ app/src/main/java/ch/epfl/sweng/opengm/polls/PollResultActivity.java
@@ -3,6 +3,8 @@ package ch.epfl.sweng.opengm.polls;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ch.epfl.sweng.opengm.R;
+
 public class PollResultActivity extends AppCompatActivity {
 
     @Override
