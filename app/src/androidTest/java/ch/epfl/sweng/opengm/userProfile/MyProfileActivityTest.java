a006ca362e8535a4c203afaa145a2c6d788a1b56
Alexandre Connat <alexandre.connat@epfl.ch>
2015-12-08 17:41:42 +0100

    Prepare to merge master on this branch

--- app/src/androidTest/java/ch/epfl/sweng/opengm/userProfile/MyProfileActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/userProfile/MyProfileActivityTest.java
@@ -3,6 +3,7 @@ package ch.epfl.sweng.opengm.userProfile;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
@@ -39,12 +40,14 @@ public class MyProfileActivityTest extends ActivityInstrumentationTestCase2<MyPr
     public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        activity = getActivity();
+
         OpenGMApplication.logOut();
+
         currentUser = PFUser.createNewUser(UtilsTest.getRandomId(), "SwaggyBoy66", "+3312131415", "alex.kuzbidon@me.com", "Alex", "Kuzbidon");
         Bitmap userPhoto = BitmapFactory.decodeResource(activity.getResources(), R.drawable.avatar_female1);
         currentUser.setPicture(userPhoto);
         currentGroup = PFGroup.createNewGroup(currentUser, "Hipster Group", "This is a group only for hipsters.", null);
+
         OpenGMApplication.setCurrentUser(currentUser);
         OpenGMApplication.setCurrentGroup(currentGroup);
     }
@@ -56,11 +59,11 @@ public class MyProfileActivityTest extends ActivityInstrumentationTestCase2<MyPr
         onView(withId(R.id.emailTV)).check(matches(withText(currentUser.getEmail())));
         onView(withId(R.id.phoneTV)).check(matches(withText(currentUser.getPhoneNumber())));
         onView(withId(R.id.descriptionTV)).check(matches(withText(currentUser.getAboutUser())));
-        if (currentUser.getPicture() == null) {
-            onView(withId(R.drawable.avatar_male1)).check(matches(isDisplayed()));
-        } else {
+//        if (currentUser.getPicture() == null) {
+//            onView(withId(R.drawable.avatar_male1)).check(matches(isDisplayed()));
+//        } else {
 //            onView(withId(R.id.userPhoto)).check(matches(isProperlyDisplayed(currentUser.getPicture())));
-        }
+//        }
     }
 
     private TypeSafeMatcher isProperlyDisplayed(final Bitmap userPhoto) {
@@ -68,14 +71,14 @@ public class MyProfileActivityTest extends ActivityInstrumentationTestCase2<MyPr
             @Override
             protected boolean matchesSafely(Object o) {
                 ImageView i = (ImageView) o;
-                Drawable photo = i.getDrawable();
-
-                Bitmap mutableBitmap = Bitmap.createBitmap(userPhoto.getWidth(), userPhoto.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(mutableBitmap);
-                photo.setBounds(0, 0, userPhoto.getWidth(), userPhoto.getHeight());
-                photo.draw(canvas);
-
+                Bitmap photo = ((BitmapDrawable) i.getDrawable()).getBitmap();
                 return photo.equals(userPhoto);
+
+//                Drawable photo = i.getDrawable();
+//                Bitmap mutableBitmap = Bitmap.createBitmap(userPhoto.getWidth(), userPhoto.getHeight(), Bitmap.Config.ARGB_8888);
+//                Canvas canvas = new Canvas(mutableBitmap);
+//                photo.setBounds(0, 0, userPhoto.getWidth(), userPhoto.getHeight());
+//                photo.draw(canvas);
             }
             @Override
             public void describeTo(Description description) {}
