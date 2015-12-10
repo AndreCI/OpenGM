d7b2f98e62e575a98eed1bcb875e580e93199029
nbonfils <nils.bonfils@epfl.ch>
2015-12-10 19:55:01 +0100

    Add some more tests for MemberActivity

--- app/src/androidTest/java/ch/epfl/sweng/opengm/groups/MemberActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/opengm/groups/MemberActivityTest.java
@@ -2,6 +2,8 @@ package ch.epfl.sweng.opengm.groups;
 
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,8 +19,12 @@ import ch.epfl.sweng.opengm.parse.PFMember;
 import ch.epfl.sweng.opengm.parse.PFUser;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.opengm.UtilsTest.deleteUserWithId;
@@ -64,7 +70,7 @@ public class MemberActivityTest extends ActivityInstrumentationTestCase2<Members
         testGroup = PFGroup.createNewGroup(user, "testGroup", "bla", null);
         OpenGMApplication.setCurrentGroup(testGroup);
 
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 1; i <= 4; i++) {
             parseUser = new ParseUser();
             parseUser.setUsername("testUsername" + i);
             parseUser.setPassword("a");
@@ -77,7 +83,7 @@ public class MemberActivityTest extends ActivityInstrumentationTestCase2<Members
             testGroup.addUserWithId(testUser.getId());
         }
 
-        for(int i = 6; i <= 10; i++) {
+        for(int i = 5; i <= 10; i++) {
             parseUser = new ParseUser();
             parseUser.setUsername("testUsername" + i);
             parseUser.setPassword("a");
@@ -127,8 +133,111 @@ public class MemberActivityTest extends ActivityInstrumentationTestCase2<Members
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertTrue(getDisplayedMembersNames().contains("testUsername10"));
-        assertFalse(getDisplayedMembersNames().contains("testUsername8"));
+        List<String> l = getDisplayedMembersNames();
+        assertTrue(l.contains("testUsername10"));
+        assertFalse(l.contains("testUsername8"));
+    }
+
+    public void testAddMemberWithEmailToGroup() {
+        onView(withId(R.id.action_add_person)).perform(click());
+        onView(withId(R.id.dialog_add_member_username)).perform(typeText("testUser" + 10 + "@testUser.com"));
+        onView(withText(R.string.add)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<String> l = getDisplayedMembersNames();
+        assertTrue(l.contains("testUsername10"));
+        assertFalse(l.contains("testUsername8"));
+    }
+
+    public void testCheckBoxAppearsOnLongClick() {
+        View v = list.getChildAt(0);
+        CheckBox c = (CheckBox)v.findViewById(R.id.member_checkbox);
+        assertTrue(c.getVisibility() == View.GONE);
+        onView(withText("testUsername")).perform(longClick());
+        assertTrue(c.getVisibility() == View.VISIBLE);
+    }
+
+    public void testCheckBoxAppearsAndCheckedOnLongClick() {
+        View v = list.getChildAt(0);
+        CheckBox c = (CheckBox)v.findViewById(R.id.member_checkbox);
+        View v2 = list.getChildAt(1);
+        CheckBox c2 = (CheckBox)v2.findViewById(R.id.member_checkbox);
+        assertTrue(c.getVisibility() == View.GONE);
+        onView(withText("testUsername")).perform(longClick());
+        assertTrue(c.getVisibility() == View.VISIBLE);
+        assertTrue(c.isChecked());
+        assertFalse(c2.isChecked());
+    }
+
+    public void testGoInSelectModeAndBack() {
+        View v = list.getChildAt(0);
+        CheckBox c = (CheckBox)v.findViewById(R.id.member_checkbox);
+        assertTrue(c.getVisibility() == View.GONE);
+        onView(withText("testUsername")).perform(longClick());
+        assertTrue(c.getVisibility() == View.VISIBLE);
+        pressBack();
+        assertTrue(c.getVisibility() == View.GONE);
+    }
+
+    public void testRemoveOneMember() {
+        List<String> l = getDisplayedMembersNames();
+        assertTrue(l.contains("testUsername4"));
+        onView(withText("testUsername4")).perform(longClick());
+        onView(withId(R.id.action_remove_person)).perform(click());
+        l = getDisplayedMembersNames();
+        assertFalse(l.contains("testUsername4"));
+        assertTrue(l.contains("testUsername3"));
+    }
+
+    public void testRemoveMultipleMembers() {
+        List<String> l = getDisplayedMembersNames();
+        assertTrue(l.contains("testUsername4"));
+        assertTrue(l.contains("testUsername1"));
+        assertTrue(l.contains("testUsername2"));
+
+        onView(withText("testUsername4")).perform(longClick());
+        onView(withText("testUsername1")).perform(click());
+        onView(withText("testUsername2")).perform(click());
+        onView(withId(R.id.action_remove_person)).perform(click());
+
+        l = getDisplayedMembersNames();
+        assertFalse(l.contains("testUsername4"));
+        assertFalse(l.contains("testUsername1"));
+        assertFalse(l.contains("testUsername2"));
+        assertTrue(l.contains("testUsername3"));
+    }
+
+    public void testAddThenRemoveMember() {
+        onView(withId(R.id.action_add_person)).perform(click());
+        onView(withId(R.id.dialog_add_member_username)).perform(typeText("testUser" + 9 + "@testUser.com"));
+        onView(withText(R.string.add)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<String> l = getDisplayedMembersNames();
+        assertTrue(l.contains("testUsername9"));
+        onView(withText("testUsername9")).perform(longClick());
+        onView(withId(R.id.action_remove_person)).perform(click());
+        l = getDisplayedMembersNames();
+        assertFalse(l.contains("testUsername9"));
+    }
+
+    public void testTryToAddWrongUsername() {
+        onView(withId(R.id.action_add_person)).perform(click());
+        onView(withId(R.id.dialog_add_member_username)).perform(typeText("bullshit"));
+        onView(withText(R.string.add)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<String> l = getDisplayedMembersNames();
+        assertFalse(l.contains("bullshit"));
     }
 
     private List<String> getDisplayedMembersNames() {
